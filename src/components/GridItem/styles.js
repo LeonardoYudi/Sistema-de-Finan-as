214c4104a21ae28410742e729dfabd50da86e188
LeonardoYudi <leonardo.iwama25@outlook.com>
2023-01-09 04:26:06 -0300

    first commit

---
+++ src/components/GridItem/styles.js
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const Tr = styled.tr``;
+
+export const Td = styled.td`
+  padding-top: 15px;
+  text-align: ${(props) => (props.alignCenter ? "center" : "start")};
+  word-break: break-all;
+  
+  svg {
+    cursor: pointer;
+    transition: all 0.3s;
+    width: 18px;
+    height: 18px;
+  }
+
+  svg:hover{
+    color: red;
+  }
+  
+`;
---
+++ src/components/ResumeItem/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import * as C from './styles'
+
+const ResumeItem = ({title,Icon, value,color}) => {
+ 
+  return (
+    <C.Container>
+      <C.Header>
+        <C.HeaderTitle>{title}</C.HeaderTitle>
+        <Icon color={color}/>
+      </C.Header>
+      <C.Total>{value}</C.Total>
+    </C.Container>
+  )
+}
+
+export default ResumeItem;
+
